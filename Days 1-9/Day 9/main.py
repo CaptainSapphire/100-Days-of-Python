9057be5fa4370bd0fbe30d99f0d7ca2551b6ceb7
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-04-25 09:29:42 -0700

    Create main.py

---
+++ Days 1-9/Day 9/main.py
@@ -0,0 +1,21 @@
+#Casting is where we explicitly tell the computer that what we are typing is a number and not a letter.
+#int and floats
+print("Welcome to the \033[33mGENERATION GENERATOR! 🥳")
+year = int(input("\033[37m What \033[31myear\033[37m were you born?"))
+
+if year >= 1925 and year <= 1946:
+  print("\033[37mYou are part of the... \033[31mTraditionalists 🕰️")
+elif year >= 1947 and year <= 1964:
+  print("\033[37mYou are part of the... \033[31mBaby Boomers 🐤💥")
+elif year >= 1965 and year <= 1981:
+  print("\033[37mYou are part of the... \033[31mGeneration 🇽")
+elif year >= 1982 and year <= 1995:
+  print("\033[37mYou are part of the... \033[31mMillenials 😂💯")
+#GEN Z UPDATED based on a google search
+elif year >= 1996 and year <= 2011:
+  print("\033[37mYou are part of the... \033[31mGeneration Z📱📚")
+elif year >= 2010 and year <= 2023:
+  print("\033[37mYou are part of the... \033[31mGeneration Alpha 🚼")
+
+
+#Felt like I was at Hogwarts with all the casting I did today 🏰🧙 ! Day 9 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
