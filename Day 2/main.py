4ff2aee69958b6624bb473f7b0473684403a56a6
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-04-25 09:23:04 -0700

    Delete Day 2 directory

--- Day 2/main.py
+++
@@ -1,7 +0,0 @@
-#DAY 2 CHALLENGE
-print("I'd like to get to know you!")
-name = input("Hello user. What is your name?")
-food = input("Cool, nice to meet you. What is your favorite food?")
-music = input("What kind of music is your favourite?")
-location = input("Where do you live?")
-print("So, your name is " + name + " ,your favorite food is " + food + "  ,your favorite music is " + music + " and you live in " + location)
