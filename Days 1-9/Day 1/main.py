eb2619b852c761e38efb6aeee83dc995952e0758
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-04-25 09:21:15 -0700

    Create main.py

---
+++ Days 1-9/Day 1/main.py
@@ -0,0 +1,20 @@
+# -----> INTRO PRACTICE
+#print("Hello Replit")
+#print("Well we")
+#print("just use more lines")
+#print("of code")
+#print("""Anything that starts
+#with three quotes, and ends
+#in three quotes can span
+#many lines and even contain " symbols
+#within it without freaking anything out!""")
+
+#print("= MUSIC+ =")
+#print("> Songs")
+#print("> Albums")
+#print("> Artists")
+# ------>
+print("Hey everyone, Priya here!")
+print("I am signing up for Replit's 100 days of Python challenge!\n I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!")
+print("I am feeling:" + " \U0001f60E")
+print("You can follow my progress at replit.com/@Priya Harry")
