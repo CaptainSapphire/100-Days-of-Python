560a1bc183b5e3009e8fe8e2511222e328adb691
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-08-26 09:51:00 -0700

    Create main.py

---
+++ Day 10-19/Day 12/main.py
@@ -0,0 +1,20 @@
+#Time to debug~!
+print("100 Days of Code QUIZ")
+print()
+print("How many can you answer correctly?")
+ans1 = input("What language are we writing in?")
+#Question 1
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈")
+#Question 2
+ans2 = int(input("Which lesson number is this?"))
+if (ans2 > 12):
+  print("We're not quite that far yet")
+elif(ans2 == 12):
+  print("That's right!")
+else:
+  print("We've gone well past that!")
+
+  #Smashed some gnarly bugs today 🐛 .  Day 12 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python
