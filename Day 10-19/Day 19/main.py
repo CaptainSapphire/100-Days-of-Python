8cb6a630d3f9757f0cd6cd183ee25ddebc3da6d0
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2024-09-09 09:40:47 -0700

    Create main.py

---
+++ Day 10-19/Day 19/main.py
@@ -0,0 +1,9 @@
+#Variables
+loan = 1000
+interest = 0.05
+#for loop
+for i in range(10):
+  loan = loan + (loan*interest)
+print(loan)
+#Directions
+#You are going to create a loan calculator that shows how much money you owe for a loan of $1,000 with a 5% APR (APR is fancy for Annual Percentage Rate) over 10 years
